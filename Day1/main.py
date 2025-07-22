ba8b2e58462e320abcde88b04efb98f2b7701cc2
maker2413 <maker2413@gmail.com>
2025-07-22 08:14:16 -0500

    made first api call and added notes

--- Day1/main.py
+++ Day1/main.py
@@ -20,3 +20,29 @@ print("OpenAI client successfully configured.")
 
 # Let's view the first few characters in the key
 print("Key begins with:", openai_api_key[:5])
+
+# Let's define the message we want to send as the 'user'
+my_message = "Write a Poem to my mom Laila congratulating her for her 74th birthday!"
+print(f"Sending message to OpenAI: '{my_message}'")
+
+# Let's make an API call to OpenAI and send our message
+response = openai_client.chat.completions.create(model = "gpt-4o-mini",
+                                                 messages = [{"role": "user", "content": my_message}])
+
+# Let's obtain the AI's reply from the response object
+# The response contains lots of info; we need to dig into it to find the text.
+# It usually looks like: response -> choices -> [first choice] -> message -> content
+ai_reply_content = response.choices[0].message.content
+
+# Let's print the reply
+print("\n🤖 AI's Reply: \n")
+print(f"{ai_reply_content}")
+
+my_message = "What is the tallest mountain in the world?"
+response = openai_client.chat.completions.create(model = "gpt-4o",
+                                                 messages = [{"role": "user", "content": my_message}])
+ai_reply_content = response.choices[0].message.content
+
+# Print the reply
+print("\n🤖 AI's Reply: \n")
+print(f"{ai_reply_content}")
